d8171217d8ff76b4afc5f664c32ba0eb3a9c2f74
Chad Smith <chad@codingbychad.com>
2022-02-01 20:56:12 -0600

    fixed earlier stupidity registration returns user & auth token

--- _users/user.service.js
+++ _users/user.service.js
@@ -1,28 +1,34 @@
 require('dotenv').config();
 const secret = process.env.SECRET;
 const jwt = require('jsonwebtoken');
-const Role = require('../utilities/role');
+const bcrypt = require('bcryptjs');
 const pg = require('../utilities/db.context');
-const { use } = require('express/lib/application');
 
 const authenticate = async ({ username, password }) => {
+  const pword = password;
   try {
-    const user = await pg.query(
-      `SELECT username, firstName, lastName, email, roles FROM users WHERE users.username=$1 AND users.password=$2`,
-      [username, password]
+    const results = await pg.query(
+      `SELECT username, password, "firstName", "lastName", email, role FROM users WHERE users.username=$1;`,
+      [username]
     );
-    if (user) {
-      const token = jwt.sign({ sub: user.id, role: user.role }, secret);
+    const u = results.rows[0];
+    if (u) {
+      if (!bcrypt.compareSync(pword, u.password)) {
+        return null;
+      }
+      const token = jwt.sign({ sub: u.id, role: u.role }, secret);
+      const { password, ...user } = u;
       return { user, token };
     }
   } catch (error) {
     console.log(`Error authenticating user.`, error);
+    return null;
   }
 };
 
 const getAll = async () => {
   const users = await pg.query(
-    `SELECT username, firstName, lastName, email, roles FROM users`,
+    `SELECT username, firstName, lastName, email, role FROM users;`,
     []
   );
   return users;
@@ -30,7 +36,7 @@ const getAll = async () => {
 
 const getById = async (userId) => {
   const user = await pg.query(
-    `SELECT username, firstName, lastName, email, roles FROM users WHERE users.id=$1`,
+    `SELECT username, firstName, lastName, email, role FROM users WHERE users.id=$1;`,
     [userId]
   );
 
@@ -39,8 +45,29 @@ const getById = async (userId) => {
   return user;
 };
 
+/**
+ * Register a new user for the application.
+ * @param {*} user
+ * @returns
+ */
+const register = async (user) => {
+  const hashedPassword = bcrypt.hashSync(user.password, 10);
+  const defaultRole = 'user';
+  const response = await pg.query(`
+    INSERT INTO users ("firstName", "lastName", "avatar_link", username, password, email, role )
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *;
+  `, [user.firstName, user.lastName, user.avatarLink, user.username, hashedPassword, user.email, defaultRole ]);
+
+  const { password, ...newUser} = response.rows[0];
+
+  return newUser;
+}
+
+
 module.exports = {
   authenticate,
   getAll,
   getById,
+  register
 };
