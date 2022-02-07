9bdd3df0251caf4878a6b5e17c56a5e85fc79b93
Chad Smith <chad@codingbychad.com>
2022-02-07 14:21:07 -0600

    rename authorize.js to authService

--- utilities/index.js
+++ utilities/index.js
@@ -1,7 +1,7 @@
-const authorize = require('./authorize');
+const authService = require('./auth.service');
 const dateFilter = require('./date-filter');
 const dbContext = require('./db.context');
 const filterPassword = require('./filter-password');
 const role = require('./role');
 
-module.exports = { authorize, dateFilter, dbContext, filterPassword, role };
+module.exports = { authService, dateFilter, dbContext, filterPassword, role };
