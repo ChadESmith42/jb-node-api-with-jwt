9b41a57293074612bcdf813ad1585c4bc86ca8a3
Chad Smith <chad@codingbychad.com>
2022-02-07 11:44:57 -0600

    add capacity checks

--- _reservations/reservations.service.js
+++ _reservations/reservations.service.js
@@ -63,12 +63,15 @@ const getResortReservations = async (resortId, startDate, endDate) => {
 
 const createReservation = async  reservation => {
   try {
-    const newReservation = await pg.query(`
-      INSERT INTO reservations (pets_id, owners_id, resorts_id, date)
-      VALUES ($1, $2, $3, $4);
-    `, [reservation.pets_id, reservation.owners_id, reservation.resorts_id, reservation.date]);
-    if (newReservation) {
-      return newReservation.rows[0];
+    const hasCapacity = await this.hasCapacity(reservation.date, reservation.resortId);
+    if (hasCapacity) {
+      const newReservation = await pg.query(`
+        INSERT INTO reservations (pets_id, owners_id, resorts_id, date)
+        VALUES ($1, $2, $3, $4);
+      `, [reservation.pets_id, reservation.owners_id, reservation.resorts_id, reservation.date]);
+      if (newReservation) {
+        return newReservation.rows[0];
+      }
     }
     return null;
   } catch (error) {
@@ -91,4 +94,86 @@ const deleteReservation = async reservationId => {
   }
 }
 
-module.exports = { getReservations, getUserReservations, getResortReservations, createReservation, deleteReservation };
+/**
+ * Get the number of existing reservations for a given date and resort.
+ * @param {date} reservationDate
+ * @param {int} reservationId
+ * @returns {int} Count of existing reservations.
+ */
+const getExistingReservationCount = async (reservationDate, reservationId) => {
+  try {
+    const reservationCount = await pg.query(
+      `
+      SELECT Count(*)
+      FROM reservations
+      WHERE resorts_id = $1 and date = $2;
+      `,
+      [reservationId, reservationDate]
+    );
+    return reservationCount.rowCount;
+  } catch (error) {
+    console.error('Could not get count of reservations.', error);
+    return null;
+  }
+}
+
+/**
+ * Gets the capacity for a given resort on a specific day of the week.
+ * @param {string} weekday
+ * @param {int} resortId
+ * @returns {int} Number of reservations possible for that day and resort.
+ */
+const getLimit = async (weekday, resortId) => {
+  const limit = await pg.query(`
+    SELECT resorts_hours.capacity
+    FROM resorts_hours
+    WHERE resorts_id = $1 AND day = $2;
+  `, [resortId, weekday]);
+  return limit.rows[0];
+}
+
+/**
+ * Determines if resort has capacity for a reservation on the date.
+ * @param {date} reservationDate
+ * @param {int} resortId
+ * @returns {boolean} True if a reservation can be made.
+ */
+const hasCapacity =  async (reservationDate, resortId) => {
+  const weekday = new Date(reservationDate).getDay();
+  const reservationDay = convertDay(weekday);
+  const capacity = await getLimit(reservationDay, resortId);
+  const booked = await getExistingReservationCount(reservationDate, resortId);
+  return booked > capacity;
+}
+
+/**
+ * Converts the integer Date.getDay() value into the string Weekday value.
+ * @param {int} weekday
+ * @returns {string} Name of the day of week.
+ */
+const convertDay = weekday => {
+  let day = '';
+  switch (weekday) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4: 'Thursday';
+      break;
+    case 5: 'Friday';
+      break;
+    case 6: 'Saturday';
+      break;
+  }
+  return day;
+}
+
+module.exports = { getReservations, getUserReservations, getResortReservations, createReservation, deleteReservation, getExistingReservationCount, hasCapacity };
