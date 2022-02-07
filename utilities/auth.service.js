9b10170b74dd3b5e4f312414a66e50b61a238346
Chad Smith <chad@codingbychad.com>
2022-02-07 14:20:44 -0600

    fix authService naming issue

---
+++ utilities/auth.service.js
@@ -0,0 +1,63 @@
+const jwt = require('jsonwebtoken');
+const role = require('./role');
+require('dotenv').config();
+const secret = process.env.SECRET;
+
+const authorize = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, secret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+      console.groupEnd();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+/**
+ * Validates user is either an Admin or an Employee.
+ * @param {object} user
+ * @returns {boolean} True if either role exists on the user object.
+ */
+const superUserOnly = user => {
+  return user.role == role.Admin || user.role == role.Employee;
+}
+
+/**
+ * Validates user is either an admin or is accessing their own records.
+ * @param {object} user
+ * @param {int} reqId
+ * @returns {boolean} True if user is either an Admin or is accessing their own records.
+ */
+const userOrAdmin = (user, reqId) => {
+  return user.role === role.Admin || (user.role === role.User && user.id === reqId);
+}
+
+/**
+ * Validates authenticated user is not an Admin or Employee.
+ * @param {object} user
+ * @returns {boolean} True if user is Admin or Employee.
+ */
+const userOnly = user => {
+  return user.role === role.User;
+}
+
+/**
+ * Validates authenticated user is an Admin.
+ * @param {object} user
+ * @returns {boolean} True is user is an Admin.
+ */
+const adminOnly = user => {
+  return user.role === role.Admin;
+}
+
+module.exports = { authorize, superUserOnly, userOrAdmin, userOnly, adminOnly };
