6c8433449e3fab2399398811258110da72e7080d
Chad Smith <chad@codingbychad.com>
2022-02-07 15:12:48 -0600

    fix typo in import

--- _pets/pets.controller.js
+++ _pets/pets.controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const petService = require('./pet.service');
+const petService = require('./pets.service');
 const { authService } = require('../utilities');
 
 
