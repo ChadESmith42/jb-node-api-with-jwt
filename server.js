d1c461e0858f4e095ef5f42fc9b64b8e8e3baa84
Chad Smith <chad@codingbychad.com>
2022-02-07 14:57:19 -0600

    add endpoints for controllers

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const employeesController = require('./_employees/employees.controller');
+const notesController = require('./_notes/notes.controller');
+const petsController = require('./_pets/pets.controller');
+const reservationsController = require('./_reservations/reservations.controller');
+const resortsController = require('./_resorts/resorts.controller');
 const userController = require('./_users/users.controller');
 
 const app = express();
@@ -10,7 +15,13 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/employees', employeesController);
+app.use('/api/notes', notesController);
+app.use('/api/pets', petsController);
+app.use('/api/reservations', reservationsController);
+app.use('/api/resorts', resortsController);
 app.use('/api/users', userController);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
