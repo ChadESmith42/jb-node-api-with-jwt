cfa82c7ab7146b2dbb652718b204b55d38d1aaf8
Chad Smith <chad@codingbychad.com>
2022-02-08 11:35:10 -0600

    reconfigure to localhost

--- utilities/db.context.js
+++ utilities/db.context.js
@@ -4,23 +4,23 @@ require('dotenv').config();
  * Uncomment the following for LOCAL development.
  */
 
-// let config = {
-//     host: process.env.PGHOST,
-//     port: process.env.PGPORT,
-//     database: process.env.PGDATABASE,
-//     max: 10, // max number of clients in the pool
-//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-// };
+let config = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
 
-// const pool = new pg.Pool(config);
+const pool = new pg.Pool(config);
 
 // Prod connection:
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// const pool = new pg.Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 /**
  * Emits an error on behalf of idle clients due to backend errors
